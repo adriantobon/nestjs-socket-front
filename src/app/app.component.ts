924249d84badafb64d1484b0a90a6c605b5e312b
Adrian Tobon Guardiola <adriantobon@Adrians-MacBook-Pro.local>
2020-01-09 18:13:43 -0600

    Emitting events to 'http://localhost:3000'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { connect } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nestjs-socket-front';
+export class AppComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) {}
+
+  mainForm: FormGroup;
+
+  messages: string[] = ['Mensaje1', 'Mensaje2'];
+  socket;
+
+  ngOnInit() {
+    this.mainForm = this.createForm();
+    this.socketInit();
+  }
+
+  createForm(): FormGroup {
+    return this.fb.group({
+      messageText: 'prueba'
+    });
+  }
+
+  sendMessage(message: string) {
+    this.socket.emit('msgToServer', message);
+  }
+
+  socketInit() {
+    this.socket = connect('http://localhost:3000');
+    this.socket.on('msgToClient', (msg) => {
+      this.messages.push(msg);
+    });
+  }
 }
